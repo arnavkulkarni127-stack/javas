079917fff7a80d4ed2940d270156a53dbf5e8ef0
arnavkulkarni127-stack <arnavkulkarni127@gmail.com>
2026-03-30 05:07:16 +0000

    operations

---
+++ java_tutorial/operations.js
@@ -0,0 +1,10 @@
+let str1 = "hello"
+let str2 = " world"
+let str3 = str1+str2
+// console.log(str3)
+
+console.log("1"+ 2) // output is 1 and 2 = 12
+console.log("1"+2+2) // same theoutput will be 122
+console.log(2+2+"1") // here thwe output is 41,  so what happened here is 2+2= 4 and the 4 and 1 ==>41
+ // per ye tarika preferred nahi hai 
+
