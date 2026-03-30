91de3e2fca73942e214d552c47b34ef6893b5b4c
arnavkulkarni127-stack <arnavkulkarni127@gmail.com>
2026-03-30 04:34:49 +0000

    datatypes

--- java_tutorial/tutorial.js
+++ java_tutorial/tutorial.js
@@ -1,1 +1,22 @@
-console.log("arnav")
+"use strict" // we are using the newer version of javascript
+
+//u should always keep your code readable
+
+// console.log("3"); console.log("56")  // this will work but is not a good practise as it does not look great
+
+// console.log("3")  // this looks good instead.
+// console.log("56")
+
+// let name = "arnav" // let me agar double quotes me data rakhe toh string ke type me store hota hai
+
+// let age = 18  // without double quotes, it is saved in the type of number.
+
+//BigInt: if number is too big
+// boolean ==> true or false
+// null ==> it is a stand alone value in javascript
+// undefined ==> means no value is given 
+// symbol ==> uniqueness
+
+console.log(typeof null) // it is an object
+console.log(typeof undefined) // not defined
+
