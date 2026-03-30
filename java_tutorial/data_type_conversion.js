7bd13e5d06d0b07f732c2a1f90e573d0368f01a5
arnavkulkarni127-stack <arnavkulkarni127@gmail.com>
2026-03-30 04:53:49 +0000

    conversion

---
+++ java_tutorial/data_type_conversion.js
@@ -0,0 +1,27 @@
+
+// {
+// let score = "33"  //the data is given in form of string 
+
+// let valueInNumber = Number(score) // here the string is converted into num type
+
+// console.table(typeof score)
+// console.log(typeof valueInNumber)
+
+// "33"==>33
+//"33abc"==>NaN
+//  }
+
+// {
+// let result = 1
+// let boolean = Boolean(result)
+// console.log(boolean)
+
+// 1==> true
+// ""==> false
+// "any thing"==>true
+// 
+// }
+ 
+let stringNumber = 33
+let string = String(stringNumber)
+console.log(typeof string)
